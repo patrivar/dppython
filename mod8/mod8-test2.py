4056e9e379de3707be7a3efbc11b39c86af20571
patrivar <patric.varis@metropolia.fi>
2024-09-12 14:10:54 +0300

    lentokenttäpalin harjoituksia

--- mod8/mod8-test2.py
+++ mod8/mod8-test2.py
@@ -20,6 +20,26 @@ def fetch_airport_by_icao(code):
     cursor.execute(sql)
     result_row = cursor.fetchone()
     #print(result_row)
+    # palauttaa monikon, paitsi jos tyhjä tulosjoukku niin -> None
     return result_row
 
-print(fetch_airport_by_icao("ZYTH"))
+user_input = input("Anna ICAO-koodi: ")
+airport = fetch_airport_by_icao(user_input)     # Käytä koodia ZYTH tai EFHK jos et muista muita
+
+# jos airport-muuttujassa on jotain muuta kuin None/False/0
+if airport: # != None:  # Antaa None jos koodia ei ole olemassa
+    print(f"Haettu lentokenttä: {airport[0]}, {airport[1]}.")
+else:
+    print("Lentokenttää ei löydetty annetulla koodilla.")
+
+# EXTRA: Tiedon lisäys
+def add_airport(icao,name, municipality):
+    sql = (f"INSERT INTO airport (id,ident, name, municipality)"
+           f"VALUES (999, '{icao}', {name}, {municipality})")
+    cursor = connection.cursor
+    cursor.execute(sql)
+
+icao = input("Anna uusi ICAO: ")
+name = input("Anna uusi kenntän nimi: ")
+municipality = input("Anna uusi paikkakunta: ")
+add_airport(icao, name, municipality)
