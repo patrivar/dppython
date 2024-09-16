618aa9e466bc15d82de4516818879df336c72659
patrivar <patric.varis@metropolia.fi>
2024-09-16 18:03:32 +0300

    lentokenttäpalin harjoituksia

---
+++ mod8/teht1.py
@@ -0,0 +1,3 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä lentoaseman ICAO-koodin. Ohjelma hakee ja tulostaa koodia
+# vastaavan lentokentän nimen ja sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
+# ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen.
---
+++ mod8/teht3.py
@@ -0,0 +1,3 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä kahden lentokentän ICAO-koodit. Ohjelma ilmoittaa
+# lentokenttien välisen etäisyyden kilometreinä. Laskenta perustuu tietokannasta haettuihin
+# koordinaatteihin. Laske etäisyys geopy-kirjaston avulla
