1eec66e04a8df10ab3ca73bd05c9d24fe9642593
patrivar <patric.varis@metropolia.fi>
2024-10-31 14:51:08 +0200

    lentokenttäpalin harjoituksia

--- mod12/teht2.py
+++ mod12/teht2.py
@@ -4,3 +4,7 @@
 # dokumentaatioon riittävästi. Palveluun rekisteröityminen on tarpeen, jotta saat
 # rajapintapyynnöissä tarvittavan API-avaimen (API key). Selvitä myös, miten saat
 # Kelvin-asteet muunnettua Celsius-asteiksi.
+
+
+# laita sivun api key 'API key' tilalle aaltosulkeiden väliin
+'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
