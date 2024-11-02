79ba03f6527b094c8377eeed4b2eb6cf836b8078
patrivar <patric.varis@metropolia.fi>
2024-11-02 13:11:39 +0200

    lentokenttäpalin harjoituksia

--- mod12/teht2.py
+++ mod12/teht2.py
@@ -7,4 +7,34 @@
 
 
 # laita sivun api key 'API key' tilalle aaltosulkeiden väliin
-'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+# 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+# 56f529fb2a62658db4ec020977c5bcc3
+
+import  geopy
+import requests
+
+def saa():
+    API_key = '56f529fb2a62658db4ec020977c5bcc3'
+    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_key}&units={temp_unit}'
+
+    try:
+        response = requests.get(url)
+
+    except requests.exceptions.RequestException as e:
+        print("Verkkovirhe")
+        print(e)
+        return
+
+    if response.status_code != 200:
+        print(f"HTTP-yhteysvirhe {response.status_code}")
+        return
+
+    response_body = requests.get(url).json()
+
+    if len(response_body) < 1:
+        print("Ei tuloksia")
+        return
+
+    print(response_body['main']['description']['temp'])
+
+saa()
