3a6588aabb112d31b1cc6ff76dceaeb7137b0e61
patrivar <patric.varis@metropolia.fi>
2024-11-02 17:20:14 +0200

    l

--- mod12/teht2.py
+++ mod12/teht2.py
@@ -8,15 +8,19 @@
 
 # laita sivun api key 'API key' tilalle aaltosulkeiden väliin
 # 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
-# 56f529fb2a62658db4ec020977c5bcc3
 
-import  geopy
 import requests
 
-def saa():
+def saa(kaupunki):
     API_key = ''
-    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_key}&units={temp_unit}'
+    temp_unit = 'metric'
 
+    units = f'http://api.openweathermap.org/geo/1.0/direct?q={kaupunki}&limit=5&appid={API_key}'
+    response1 = requests.get(units).json()
+    lat, lon = response1['lat'], ['lon']
+    print(lat, lon)
+    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_key}&units={temp_unit}'
+    # &exclude={part}
     try:
         response = requests.get(url)
 
@@ -35,6 +39,7 @@ def saa():
         print("Ei tuloksia")
         return
 
-    print(response_body['main']['description']['temp'])
+    print(response_body)
+    #print(response_body['main']['description']['temp'])
 
-saa()
+saa(input("Kaupunki:"))
