6df1aa60b205d111311989ccf7bc53952be1615f
patrivar <patric.varis@metropolia.fi>
2024-11-05 13:13:02 +0200

    l

--- mod12/teht2.py
+++ mod12/teht2.py
@@ -6,12 +6,10 @@
 # Kelvin-asteet muunnettua Celsius-asteiksi.
 
 
-# laita sivun api key 'API key' tilalle aaltosulkeiden väliin
-# 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
-
 import requests
 
 def saa(kaupunki):
+    # Laita api avaimesi merkkien väliin
     API_key = ''
     temp_unit = 'metric'
 
@@ -41,7 +39,6 @@ def saa(kaupunki):
         return
 
     print(response_body)
-
 
 
 saa(input("Kaupunki:"))
