5b121fecc707d9f06b6b57aa0d156ebf68c8f4b7
patrivar <patric.varis@metropolia.fi>
2024-11-07 09:55:33 +0200

    l

--- mod13/teht2.py
+++ mod13/teht2.py
@@ -1,6 +1,6 @@
 #Toteuta taustapalvelu, joka palauttaa annettua lentokentän ICAO-koodia vastaavan lentokentän nimen
 # ja kaupungin JSON-muodossa. Tiedot haetaan opintojaksolla käytetystä lentokenttätietokannasta.
-# Esimerkiksi EFHK-koodia vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/kenttä/EFHK.
+# Esimerkiksi EFHK-koodia vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/kentta/EFHK.
 # Vastauksen on oltava muodossa: {"ICAO":"EFHK", "Name":"Helsinki Vantaa Airport", "Municipality":"Helsinki"}.
 
 from flask import Flask, request
