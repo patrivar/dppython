a2b9277be230b56740edb868c921c9645358bf6b
patrivar <patric.varis@metropolia.fi>
2024-11-09 15:22:28 +0200

    l

--- mod13/teht2.py
+++ mod13/teht2.py
@@ -3,16 +3,52 @@
 # Esimerkiksi EFHK-koodia vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/kentta/EFHK.
 # Vastauksen on oltava muodossa: {"ICAO":"EFHK", "Name":"Helsinki Vantaa Airport", "Municipality":"Helsinki"}.
 
-from flask import Flask, request
+from flask import Flask, jsonify
+import mysql.connector
+
+connection = mysql.connector.connect(
+        host='127.0.0.1', #localhost
+        port=3306,
+        database='flight_game',
+        user='flight_game',
+        password='flight_game',
+        autocommit=True,
+        collation='utf8mb4_general_ci',
+        )
+
+def fetch_airport_by_icao(code):
+    sql = (f"SELECT name, municipality "
+           f"FROM airport WHERE ident='{code}'")
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result_row = cursor.fetchone()
+    return result_row
+
 
 app = Flask(__name__)
 @app.route('/kentta/<icao>')
 def haekenttä(icao):
-    try:
+    user_input = icao
+    airport = fetch_airport_by_icao(user_input)
 
+    try:
         tilakoodi = 200
         vastaus = {
+            "ICAO": icao,
+            "Name": airport[0],
+            "Municipality":airport[1]
         }
+
+    except ValueError:
+        tilakoodi = 400
+        vastaus = {
+            "status": tilakoodi,
+            "teksti": "Virheellinen syöte"
+        }
+
+    return jsonify(vastaus)
+
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
