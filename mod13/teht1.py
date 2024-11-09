7300c396bcc178f6b0d6d6808d18ff10c9327a0e
patrivar <patric.varis@metropolia.fi>
2024-11-09 15:05:52 +0200

    l

--- mod13/teht1.py
+++ mod13/teht1.py
@@ -3,23 +3,26 @@
 # vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31. Vastauksen on oltava
 # muodossa: {"Number":31, "isPrime":true}.
 
-from flask import Flask, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-@app.route('/alkuluku/<luku>')
+
+def laske(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+@app.route('/alkuluku/<int:luku>', methods=['GET'])
 def alkuluku(luku):
-    try:
-        luku = int(alkuluku)
-        alkuluku = teht1_2.laske(luku)
-
-        tilakoodi = 200
-
-        vastaus = {
-            "number": luku
-            "isPrime": alkuluku
-        }
-
-
+    tulos = laske(luku)
+    vastaus = {
+        "Number": luku,
+        "isPrime": tulos
+    }
+    return jsonify(vastaus)
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
--- mod13/teht1_2.py
+++
@@ -1,6 +0,0 @@
-def laske(luku):
-    result = True
-    for i in range(2, luku):
-        if luku % i == 0:
-            return False
-    return result
